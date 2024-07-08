96b1d45f42b6878b6fe086c35573b392079a0496
mahen <oresuki01@gamil.com>
2024-07-08 11:20:03 +0700

    init error handler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -3,7 +3,7 @@ const errorHandler = (err, req, res, next) => {
     console.log(err);
 
     switch (err.name) {
-        //Register & Login
+        //Auth error
         case 'emailAlreadyExist':
             errorMessage = 'Email Already Exist';
             res.status(400).json({ message: errorMessage });
@@ -32,6 +32,137 @@ const errorHandler = (err, req, res, next) => {
         case 'unauthorized':
             errorMessage = 'Authorization Required'
             res.status(401).json({ message: errorMessage });
+
+        // Base error
+        case 'notFound':
+            errorMessage = 'Error Not Found'
+            res.status(404).json({ message: errorMessage });
+            break;
+
+        //Cart error
+        case 'cartNotFound':
+            errorMessage = 'Cart Id Not Found'
+            res.status(404).json({ message: errorMessage });
+            break;
+
+        //User error
+        case 'userNotFound':
+            errorMessage = 'User Not Found'
+            res.status(404).json({ message: errorMessage });
+            break;
+
+        case 'failedToUpdateUser':
+            errorMessage = 'Failed to Update User'
+            res.status(400).json({ message: errorMessage });
+            break;
+
+        //Product error
+        case 'productNotFound':
+            errorMessage = 'Product Not Found'
+            res.status(404).json({ message: errorMessage });
+            break;
+
+        case 'failedToCreateProduct':
+            errorMessage = 'Failed to Create Product'
+            res.status(400).json({ message: errorMessage });
+            break;
+
+        case 'failedToCreateProductImage':
+            errorMessage = 'Failed to Create Product Image'
+            res.status(400).json({ message: errorMessage });
+            break;
+
+        case 'failedToUpdateProduct':
+            errorMessage = 'Failed to Update Product'
+            res.status(400).json({ message: errorMessage });
+            break;
+
+        case 'failedToDeleteProduct':
+            errorMessage = 'Failed to Delete Product'
+            res.status(400).json({ message: errorMessage });
+            break;
+
+        //Category error
+        case 'categoriesNotFound':
+            errorMessage = 'category Not Found'
+            res.status(404).json({ message: errorMessage });
+            break;
+
+        case 'updateCategoryFailed':
+            errorMessage = 'Update Category Failed'
+            res.status(400).json({ message: errorMessage });
+            break;
+
+        //City error
+        case 'cityNotFound':
+            errorMessage = 'City Not Found'
+            res.status(404).json({ message: errorMessage });
+            break;
+
+        //Province error
+
+        case 'provinceNotFound':
+            errorMessage = 'Province Not Found'
+            res.status(404).json({ message: errorMessage });
+            break;
+
+        //Cart error
+        case 'cartNotFound':
+            errorMessage = 'Cart Not Found'
+            res.status(404).json({ message: errorMessage });
+            break;
+
+        case 'failedUpdateCart':
+            errorMessage = 'Failed to Update Cart'
+            res.status(400).json({ message: errorMessage });
+            break;
+
+        case 'failedTOResetCart':
+            errorMessage = 'Failed to Reset Cart'
+            res.status(400).json({ message: errorMessage });
+            break;
+
+        case 'failedTODeleteCart':
+            errorMessage = 'Failed to Delete Cart'
+            res.status(400).json({ message: errorMessage });
+            break;
+
+        // Order error
+        case 'orderNotFound':
+            errorMessage = 'Order Not Found'
+            res.status(404).json({ message: errorMessage });
+            break;
+
+        case 'failedToFindAllOrder':
+            errorMessage = 'Failed to Find All Order'
+            res.status(400).json({ message: errorMessage });
+            break;
+
+        case 'failedToUpdatestatus':
+            errorMessage = 'Failed to Update Status Order'
+            res.status(400).json({ message: errorMessage });
+            break;
+
+        case 'paymentError':
+            errorMessage = 'Payment Error'
+            res.status(400).json({ message: errorMessage });
+            break;
+
+        case 'handleNotificationError':
+            errorMessage = 'Handle Notification Error'
+            res.status(400).json({ message: errorMessage });
+            break;
+
+        //Stock error
+        case 'failedToAddStock':
+            errorMessage = 'Failed to Add Stock'
+            res.status(400).json({ message: errorMessage });
+            break;
+
+        case 'failedToGetStock':
+            errorMessage = 'Failed to Get Stock'
+            res.status(400).json({ message: errorMessage });
+            break;
 
 
         default:
