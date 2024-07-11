0b5e5e7eafad1f98421b73b75cd63839976da064
mahen <oresuki01@gamil.com>
2024-07-11 12:23:26 +0700

    edit: status-500

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -20,7 +20,7 @@ const errorHandler = (err, req, res, next) => {
     } else if (err.name === 'failedToDelete') {
         res.status(400).json({ message: err.message });
     } else {
-        res.status(500).json({ message: err.message });
+        res.status(500).json({ message: 'Internal Server Error'});
     }
 }
 
