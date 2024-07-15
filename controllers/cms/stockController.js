2ce897281fdcf879e6b0c16828cd934af27214fa
mahen <oresuki01@gamil.com>
2024-07-15 12:25:50 +0700

    feat: stock

--- controllers/cms/stockController.js
+++ controllers/cms/stockController.js
@@ -1,5 +1,54 @@
-const addStock = async (req, res, next) => {}
+const stockService = require('../../services/stockService');
 
-const getStock = async (req, res, next) => {}
+const addStock = async (req, res, next) => {
+    try {
+        const params = { ...req.body }
 
-module.exports = {addStock, getStock};
+        const data = await stockService.addStock(params);
+
+        res.status(200).json({
+            message: 'Adding stock success',
+            data: data
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+const updateStock = async (req, res, next) => {
+    try {
+        const params = {
+            product_id: +req.params.product_id,
+            ...req.body
+        }
+
+        const data = await stockService.updateStock(params);
+
+        res.status(200).json({
+            message: 'Update Stock Success',
+            data: data
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+const getStock = async (req, res, next) => {
+    try {
+        const params = {
+            warehouse_id: req.params.warehouse_id,
+            product_id: req.body.product_id
+        }
+
+        const data = await stockService.getStock(params)
+
+        res.status(200).json({
+            message: 'Get Stock Success',
+            data: data
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = { addStock, getStock, updateStock };
--- routes/cms/stockRoute.js
+++ routes/cms/stockRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const stockController = require('../../controllers/cms/stockController');
 
 router.post('/', stockController.addStock);
+router.put('/:product_id', stockController.updateStock);
 router.get('/:warehouse_id', stockController.getStock);
 
 module.exports = router;
