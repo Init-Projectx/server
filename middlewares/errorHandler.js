8c74c680d5e9f4335eabad10e229819443657d5e
Mahen <166118742+bebek-goreng@users.noreply.github.com>
2024-07-16 16:31:43 +0700

    Migration (#26)
    * feat: province
    
    * fix: seed file name
    
    * fix: auth
    
    * feat: users
    
    * edit: error-handler
    
    * edit: add-field-status
    
    * edit: add-field-status
    
    * add address field in order
    
    ---------
    
    Co-authored-by: mahen <oresuki01@gamil.com>

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -21,7 +21,7 @@ const errorHandler = (err, req, res, next) => {
     } else if (err.name === 'failedToDelete') {
         res.status(400).json({ message: err.message });
     } else {
-        res.status(500).json({ message: 'Internal Server Error'});
+        res.status(500).json({ message: 'Internal Server Error' });
     }
 }
 
