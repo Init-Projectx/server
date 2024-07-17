736e76015e5c72f1cd3f31afc2b6fababda9ab4a
Azmi117 <131350266+Azmi117@users.noreply.github.com>
2024-07-17 09:58:46 +0700

    Feature/product (#24)
    * feature/product
    
    * feature/product
    
    * feature/product
    
    * feature/product
    
    ---------
    
    Co-authored-by: azmiyushari <azmiyushrr@gmail.com>

--- routes/cms/productRoute.js
+++ routes/cms/productRoute.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const productController = require('../../controllers/cms/productController');
+const upload = require('../../middlewares/multerProduct');
 
 router.get('/', productController.findAll);
-router.get('/:id', productController.findOne);
-router.post('/', productController.create);
-router.put('/:id', productController.update);
-router.delete('/:id', productController.destroy);
+router.get('/:slug', productController.findOne);
+router.get('/category/:categoryId', productController.findByCategory);
+router.post('/', upload, (req, res, next) => {
+    req.err = req.err || null;
+    next();
+}, productController.create);
+router.put('/:slug', upload, (req, res, next) => {
+    req.err = req.err || null;
+    next();
+}, productController.update);
+router.delete('/:slug', productController.softDelete);
+router.put('/return/:slug', productController.returnSoftDelete);
 
 module.exports = router;
