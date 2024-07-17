6c46de90fe9c8514d0274ed317541eda3d6b9f74
Mahen <166118742+bebek-goreng@users.noreply.github.com>
2024-07-18 00:38:58 +0700

    Feature/cart (#19)
    * feat: province
    
    * fix: seed file name
    
    * fix: auth
    
    * feat: users
    
    * edit: error-handler
    
    * edit: status-500
    
    * back up
    
    * feat: cart
    
    * edit: delete auth file
    
    * edit: feature cart
    
    ---------
    
    Co-authored-by: mahen <oresuki01@gamil.com>

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,9 +1,91 @@
-const findOne = async (req, res, next) => {}
+const { warehouse, cart_items } = require('../lib/prisma');
+const cartService = require('../services/cartService');
 
-const update = async (req, res, next) => {}
+const findOne = async (req, res, next) => {
+    try {
+        const params = {
+            id: +req.params.id,
+            user: req.loggedUser.id
+        }
 
-const reset = async (req, res, next) => {}
+        const data = await cartService.findOne(params);
 
-const deleteProduct = async (req, res, next) => {}
+        res.status(200).json({
+            message: 'Get Cart by id Success',
+            data: data
+        });
+    } catch (error) {
+        next(error);
+    }
+}
 
-module.exports = {findOne, update, reset, deleteProduct};
+const update = async (req, res, next) => {
+    try {
+        const params = {
+            user_id: req.loggedUser.id,
+            ...req.body
+        }
+
+        const data = await cartService.update(params);
+
+        res.status(200).json({
+            message: 'Update Cart Success',
+            data: data
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+const reset = async (req, res, next) => {
+    try {
+        const params = {
+            user_id: req.loggedUser.id
+        }
+
+        const data = await cartService.reset(params);
+
+        res.status(200).json({
+            message: 'Reset Cart Success',
+            data: data
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+const deleteProduct = async (req, res, next) => {
+    try {
+        const params = {
+            user_id: req.loggedUser.id,
+            product_id: req.params.product_id
+        }
+
+        const data = await cartService.deleteProduct(params);
+
+        res.status(200).json({
+            message: 'Delete cart items success'
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+const getShippingCost = async (req, res, next) => {
+    try {
+        const params = {
+            ...req.body
+        }
+
+        const data = await cartService.getShippingCost(params);
+
+        res.status(200).json({
+            message: 'Get shipping cost success',
+            data: data
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = { findOne, update, reset, deleteProduct, getShippingCost };
