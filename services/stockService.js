85168a9ae4ffe5e950fd028bfb0e738ca28f0e9a
Mahen <166118742+bebek-goreng@users.noreply.github.com>
2024-07-18 00:50:32 +0700

    Feature/stock (#31)
    * feat: province
    
    * fix: seed file name
    
    * fix: auth
    
    * feat: users
    
    * edit: error-handler
    
    * edit: status-500
    
    * back up
    
    * feat: add stock
    
    * feat: stock
    
    * Revert "back up"
    
    This reverts commit e236b397e50b0c71dd678f3a567227f1a5235d2a.
    
    * feat: stock
    
    ---------
    
    Co-authored-by: mahen <oresuki01@gamil.com>
    Co-authored-by: Diah Nofitasari <diahnof@gmail.com>

--- services/stockService.js
+++ services/stockService.js
@@ -1,7 +1,65 @@
-const prisma = require('../lib/prisma');
+const prisma = require("../lib/prisma");
 
-const addStock = async (params) => {};
+const addStock = async (params) => {
+    const existingProduct = await prisma.product_Warehouse.findFirst({
+        where: {
+            AND: [
+                { product_id: params.product_id },
+                { warehouse_id: params.warehouse_id }
+            ]
+        }
+    });
 
-const getStock = async (params) => {};
+    if (existingProduct) throw { name: 'exist', message: 'Product already exist, you can update product stock' }
 
-module.exports = { addStock, getStock };
+    const data = await prisma.product_Warehouse.create({
+        data: {
+            product_id: params.product_id,
+            warehouse_id: params.warehouse_id,
+            stock: params.stock
+        }
+    });
+
+    return data;
+};
+
+const updateStock = async (params) => {
+    const product = {
+        product_id: params.product_id,
+        warehouse_id: params.warehouse_id
+    };
+
+    const existingProduct = await getStock(params);
+
+    if (!existingProduct) throw { name: 'notFound', message: 'Product Not Found in Warehouse' }
+
+    const data = await prisma.product_Warehouse.update({
+        where: {
+            id: existingProduct.id
+        }, data: {
+            stock: params.stock
+        }
+    });
+
+    if (!data) throw { name: 'notFound', message: 'Product Not Found in the specified Warehouse' }
+
+    return data;
+};
+
+
+const getStock = async (params) => {
+    const data = await prisma.product_Warehouse.findFirst({
+        where: {
+            AND: [
+                { warehouse_id: +params.warehouse_id },
+                { product_id: params.product_id }
+            ]
+        }
+    });
+
+    if (!data) throw { name: 'notFound', message: 'Error not found' };
+
+    return data;
+};
+
+module.exports = { addStock, getStock, updateStock };
